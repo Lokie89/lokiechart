f8dcc72491717fef903ae663c4024b4c1b39996a
Lokie89 <traeuman@gmail.com>
2021-04-13 13:24:41 +0900

    init

---
+++ src/test/java/com/lokiechart/www/CoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokiechart.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
