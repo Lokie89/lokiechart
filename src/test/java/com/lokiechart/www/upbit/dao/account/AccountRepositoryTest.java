1caaf9a38ed5167a017fbfc07f363bdfe199d5db
Lokie89 <traeuman@gmail.com>
2021-04-14 18:45:47 +0900

    test : 계정 자산 정보 가져오기

--- src/test/java/com/lokiechart/www/upbit/dao/account/AccountRepositoryTest.java
+++ src/test/java/com/lokiechart/www/upbit/dao/account/AccountRepositoryTest.java
@@ -1,14 +1,9 @@
 package com.lokiechart.www.upbit.dao.account;
 
-import com.lokiechart.www.upbit.dao.account.dto.AssetResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author SeongRok.Oh
@@ -21,9 +16,10 @@ class AccountRepositoryTest {
     @Autowired
     AccountRepository repository;
 
+    @DisplayName("자산 정보 가져오기")
     @Test
-    void connectTest() {
+    void getAssetsTest() {
         String account = "tjdfhrdk10@naver.com";
-        List<AssetResponse> assetResponseList = repository.getAssets(account);
+        repository.getAssets(account);
     }
 }
