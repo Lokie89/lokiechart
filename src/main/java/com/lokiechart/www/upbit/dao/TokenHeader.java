5fd03f49190ff6d93ec79c7def66d3164b184e72
Lokie89 <traeuman@gmail.com>
2021-04-15 03:30:19 +0900

    feat : 토큰 값 포함 헤더 클래스

---
+++ src/main/java/com/lokiechart/www/upbit/dao/TokenHeader.java
@@ -0,0 +1,37 @@
+package com.lokiechart.www.upbit.dao;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * @author SeongRok.Oh
+ * @since 2021/04/15
+ */
+@PropertySource("classpath:application-secret.yml")
+@Component
+public class TokenHeader {
+    @Value("${accessKey}")
+    private String accessKey;
+    @Value("${secretKey}")
+    private String secretKey;
+
+    public HttpHeaders getHeaders(String account) {
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        String jwtToken = JWT.create()
+                .withClaim("access_key", accessKey)
+                .withClaim("nonce", UUID.randomUUID().toString())
+                .sign(algorithm);
+
+        String authenticationToken = "Bearer " + jwtToken;
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        headers.add("Authorization", authenticationToken);
+        return headers;
+    }
+}
--- src/main/java/com/lokiechart/www/upbit/dao/account/AccountRepository.java
+++ src/main/java/com/lokiechart/www/upbit/dao/account/AccountRepository.java
@@ -1,47 +1,24 @@
 package com.lokiechart.www.upbit.dao.account;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
 import com.lokiechart.www.upbit.dao.CallByApi;
+import com.lokiechart.www.upbit.dao.TokenHeader;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 /**
  * @author SeongRok.Oh
  * @since 2021/04/13
  */
 @RequiredArgsConstructor
-@PropertySource("classpath:application-secret.yml")
 @Component
 public class AccountRepository {
 
-    @Value("${accessKey}")
-    private String accessKey;
-    @Value("${secretKey}")
-    private String secretKey;
-
+    private final TokenHeader tokenHeader;
     private final CallByApi api;
 
     public String getAssets(String account) {
         final String serverUrl = "https://api.upbit.com";
-
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
-        String jwtToken = JWT.create()
-                .withClaim("access_key", accessKey)
-                .withClaim("nonce", UUID.randomUUID().toString())
-                .sign(algorithm);
-
-        String authenticationToken = "Bearer " + jwtToken;
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json");
-        headers.add("Authorization", authenticationToken);
-
-        return api.get(serverUrl + "/v1/accounts", headers);
+        return api.get(serverUrl + "/v1/accounts", tokenHeader.getHeaders(account));
     }
 
 }
