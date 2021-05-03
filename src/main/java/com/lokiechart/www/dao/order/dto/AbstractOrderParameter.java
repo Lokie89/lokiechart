9ae26c59dab6fb36f117175a7d910afae3283637
Lokie89 <traeuman@gmail.com>
2021-05-03 10:54:40 +0900

    refactor : OrderParameter toParameter Null 값 필터링 로직 적용

--- src/main/java/com/lokiechart/www/dao/order/dto/AbstractOrderParameter.java
+++ src/main/java/com/lokiechart/www/dao/order/dto/AbstractOrderParameter.java
@@ -2,8 +2,10 @@ package com.lokiechart.www.dao.order.dto;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Maps;
 
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author SeongRok.Oh
@@ -14,7 +16,10 @@ public abstract class AbstractOrderParameter implements OrderParameter {
 
     @Override
     public Map<String, Object> toParameter() {
-        return objectMapper.convertValue(this, new TypeReference<>() {});
+        Map<String, Object> params = objectMapper.convertValue(this, new TypeReference<>() {});
+        params = Maps.filterValues(params, Objects::nonNull);
+        System.out.println(params);
+        return params;
     }
 
     public abstract String getMarket();
--- src/test/java/com/lokiechart/www/dao/order/UpbitOrderRepositoryTest.java
+++ src/test/java/com/lokiechart/www/dao/order/UpbitOrderRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.lokiechart.www.dao.order;
 
+import com.lokiechart.www.dao.order.dto.OrderSide;
+import com.lokiechart.www.dao.order.dto.OrderType;
+import com.lokiechart.www.dao.order.dto.UpbitOrderParameter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +22,7 @@ class UpbitOrderRepositoryTest {
     @DisplayName("주문 테스트")
     @Test
     void orderTest() {
-//        UpbitOrderParameter request = UpbitOrderParameter.builder().market("KRW-AHT").side(UpbitOrderSide.BUY).volume(5000.0).price(1.0).orderType(UpbitOrderType.LIMIT).build();
+//        UpbitOrderParameter request = UpbitOrderParameter.builder().market("KRW-BTT").side(OrderSide.SELL).volume(10000.0).orderType(OrderType.DOWNERMARKET).build();
 //        String result = upbitOrderRepository.order("tjdfhrdk10@naver.com", request);
 //        System.out.println(result);
     }
